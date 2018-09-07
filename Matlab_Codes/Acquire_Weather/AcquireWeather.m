017d6abe72e1975c605a1fe14488637c91015c58
Mohamed Eldakadosi <42766785+eldakadosi@users.noreply.github.com>
2018-09-07 21:45:40 +0200

    Create AcquireWeather.m

---
+++ Matlab_Codes/Acquire_Weather/AcquireWeather.m
@@ -0,0 +1,24 @@
+
+country='Germany';
+city='Munich';
+WeatherAPI= 000000000000;
+AcqServer=['http://api.wunderground.com/api/' WeatherAPI '/hourly10day/q/'];
+%%% Acquire data online
+url=[AcqServer country '/' city '.json'];
+
+
+contents = urlread(url);
+data = parse_json(contents);
+
+
+contents = urlread(url);
+dataAstro = parse_json(contents);
+
+%%%% Structuring it with the acquiring time for safety
+DataOut=struct('acquiringTime',datetime('now'),'data',data,'Astro',dataAstro);
+
+%%% Saving file
+Dir='C:\Weather_Forecasts\';
+filename=[Dir datestr(clock,30) '_hourlyforecast_10days.mat'];
+
+save(filename,'DataOut')
