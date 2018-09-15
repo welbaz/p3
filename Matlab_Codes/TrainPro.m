489be25d9e12951c28c80eab80a959ba6962ea04
Mohamed Eldakadosi <42766785+eldakadosi@users.noreply.github.com>
2018-09-16 00:35:32 +0200

    Update TrainPro.m

--- Matlab_Codes/TrainPro.m
+++ Matlab_Codes/TrainPro.m
@@ -1,7 +1,4 @@
-function [Distrib]=TrainPro(Data)
-%this function produces the best fitting CDF for the data of
-%each cloudness levels [0/8 1/8 ... 8/8]
-
+function [Distrib]=TrainPro(Data)    %input Data: [Weather_Condition Diff]
 Data=sortrows(Data);            %Sort due to Weather_Condition
 %PDout=[];
 j=1;
@@ -12,20 +9,23 @@ for i=1:9        %Search for probability distribution for all 9 cloudeness level
 if isempty(x)
     clear PD
     PD=NaN;        %if this cloudness level doesn't exist
-    warning(['cloudness level ' num2str(x) '/8 is not available in trained data. ' ...
+    warning(['cloudness level ' num2str(i-1) '/8 is not available in trained data. ' ...
         'This will affect the quality of the prediction.'])
+    %Distrib(j)=PD(1,1);
 elseif ~any(~isnan(x(:)))
     clear PD
     PD=NaN;
-    warning(['cloudness level ' num2str(x) '/8 is not available in trained data. ' ...
+    warning(['cloudness level ' num2str(i-1) '/8 is not available in trained data. ' ...
         'This will affect the quality of the prediction.'])
+    %Distrib(j)=PD(1,1);
 else
     Data10=Data(x,2);           %Get all Diff-Data belonging to Weather-Condition
     clear x D PD
-    [D, PD] = allfitdist_woFig(Data10, 'CDF');    %allfitdist searches for best fitting CDF to the Data 
+    [D, PD] = allfitdist_woFig(Data10, 'CDF');    %allfitdist searches for best fittin comulatice distribution function to the Data 
+    Distrib(j)=PD{1,1};
 end
 
-Distrib(j)=PD{1,1};
+%Distrib(j)=PD{1,1};
 %PDout=[PDout; PD];
  j=j+1;
 end
